ab85ee2b66b4d33b6cbe4dbcf1ea65823f69458b
CaseyKelly <casey.kelly@colorado.edu>
2015-02-23 09:55:28 -0700

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path, notice: "Thanks for signing up, dude!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, notice: 'User was successfully deleted.'
+    end
+  end
+
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:user_name, :about, :password, :password_confirmation)
+    end
+
 end
