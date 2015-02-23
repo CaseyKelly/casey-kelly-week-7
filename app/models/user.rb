eb6c35b7e075d2ea248901807385ab78cbfec1af
CaseyKelly <casey.kelly@colorado.edu>
2015-02-23 13:08:59 -0700

    Users can edit users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :about, :password, presence: true
+  validates :about, presence: true
   validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 5, maximum: 120}, on: :create
 end
